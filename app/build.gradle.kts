997881c713063f6249497634466e74b7ec2851b2
Raffaello Di Martino <raffaello.dimartino@kwos.org>
2025-03-20 23:24:08 +0100

    Aggiornata versione

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kwos.dronepilotapp"
         minSdk = 27
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
